20268df15112ebcabada22c7ddbcacdf434baa1e
Fernando-l-o-s <ferzinandu@gmail.com>
2023-07-14 22:19:42 -0300

    bootstrap.start

--- Components/header.php
+++ Components/header.php
@@ -1,7 +1,8 @@
-<header>
-    <a href="../views/home.php"><img src="../img/logo.png" tittle="logo" alt="logo" height=90 ></a>
+<header class="header">
+    <a href="../views/home.php"><img src="../img/logo.png" tittle="logo" alt="logo" height=90></a>
     <div id="menu">
-        <a href="../views/home.php"> Home</a>
         <a href="../views/livros.php"> Livros</a>
+        <a class="btn btn-primary" href="../views/home.php" role="button">Home</a>
+
     </div>
 </header>
--- views/home.php
+++ views/home.php
@@ -1,12 +1,14 @@
 <?php
 require '../controllers/db.php';
 
-$query = $conexao->prepare("SELECT * FROM LIVROS");
-
+if (isset($_POST['pesquisa'])) {
+    $pesquisa = $_POST['pesquisa'];
+    $query = $conexao->prepare("SELECT * FROM LIVROS WHERE categoria LIKE '%$pesquisa%'");
+} else {
+    $query = $conexao->prepare("SELECT * FROM LIVROS");
+}
 $query->execute();
-
 $resultado = $query->fetchAll(PDO::FETCH_ASSOC);
-
 ?>
 
 <!DOCTYPE html>
@@ -17,12 +19,25 @@ $resultado = $query->fetchAll(PDO::FETCH_ASSOC);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mangateca home</title>
     <link rel="stylesheet" href="../css/index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 
 <body>
     <?php include '../components/header.php'; ?>
+
+    <div class="row">
+        <form action="home.php" method="post">
+            <div class="col-12 col-md-6 d-flex p-2  mx-auto">
+                <label class="form-label" for="input1"></label>
+                <input name="pesquisa" type="search" class="form-control" id="input1">
+                <button type="submit" class="btn btn-primary mx-3"> Pesquisa</button>
+            </div>
+        </form>
+    </div>
+
     <div class="corposite">
-        <table class="tabela">
+
+        <table class="table table-striped">
             <thead>
                 <tr>
                     <th>Titulo</th>
@@ -45,7 +60,12 @@ $resultado = $query->fetchAll(PDO::FETCH_ASSOC);
             </tbody>
         </table>
     </div>
+
+
     <?php include '../components/footer.php'; ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
 </body>
 
 </html>
